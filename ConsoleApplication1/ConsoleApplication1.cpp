0a78f126f41a5bee877fe687b62c0b16a7eb909d
hazzakak <smith.harry1807@hotmail.co.uk>
2022-12-01 10:15:03 +0000

    Documentation on main file.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,29 +7,19 @@ using namespace std;
 
 int main()
 {
+    // Initialise the A object.
     A a1 = A(2);
     A a2 = A(3);
     A a3 = A(5);
 
-    a1.print();
-    a2.print();
-    a3.print();
+    a1.print();// "My value is 2"
+    a2.print();// "My value is 3"
+    a3.print();// "My value is 5"
 
-    A a4 = a1+a2+a3;
-    A a5 = a4++;
+    A a4 = a1+a2+a3; // adds 2+3+5 == 10
+    A a5 = a4++; // incrememnts a4
     a4.print(); // "My value is 10"
     a5.print(); // "My value is 11"
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
